d30eaad958176e440d6760dbba3779f060695227
Patrick Saitta <ptsaitta@gmail.com>
2020-12-11 14:26:11 -0500

    updated primary key identification syntax in schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,34 +1,40 @@
 CREATE TABLE Departments (
 dept_name varchar(100),
-dept_no PK varchar(6)
+dept_no varchar(6),
+PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE Department_Employees (
-dept_no PK varchar(6) FK >- Departments.dept_no,
-emp_no PK int FK >- Employees.emp_no
+dept_no varchar(6) FK >- Departments.dept_no,
+emp_no int FK >- Employees.emp_no,
+PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE Department_Manager (
-dept_no PK varchar(6) FK >- Departments.dept_no,
-emp_no PK int
+dept_no varchar(6) FK >- Departments.dept_no,
+emp_no int,
+PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE Employees (
-emp_no PK int,
+emp_no int,
 emp_title_id varchar(5),
 birth_date date,
 first_name varchar(30),
 last_name varchar(30),
 sex varchar(1),
-hire_date date
+hire_date date,
+PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE Salaries (
-emp_no PK int FK >- Employees.emp_no,
-salary int
+emp_no int FK >- Employees.emp_no,
+salary int,
+PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE Titles (
-title_id PK varchar(5) FK >- Employees.emp_title_id,
-title string
+title_id varchar(5) FK >- Employees.emp_title_id,
+title string,
+PRIMARY KEY (title_id)
 );
