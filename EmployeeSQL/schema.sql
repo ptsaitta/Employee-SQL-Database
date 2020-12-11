5e8d44a85bdd0828b7516189230e9dc2d0ecdd35
Patrick Saitta <ptsaitta@gmail.com>
2020-12-11 18:12:51 -0500

    fixed foreign keys, really

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -16,19 +16,16 @@ PRIMARY KEY (dept_no)
 CREATE TABLE Department_Employees (
 dept_no varchar(6),
 emp_no int,
-PRIMARY KEY (dept_no, emp_no)
-CONSTRAINT fk_dept_no
-	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
-CONSTRAINT fk_emp_no
-	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+PRIMARY KEY (dept_no, emp_no),
+FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
 CREATE TABLE Department_Manager (
 dept_no varchar(6),
 emp_no int,
 PRIMARY KEY (dept_no, emp_no),
-CONSTRAINT fk_dept_no
-	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
 );
 
 CREATE TABLE Employees (
@@ -46,14 +43,12 @@ CREATE TABLE Salaries (
 emp_no int,
 salary int,
 PRIMARY KEY (emp_no),
-CONSTRAINT fk_emp_no
-	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
 CREATE TABLE Titles (
 title_id varchar(5),
 title string,
 PRIMARY KEY (title_id),
-CONSTRAINT fk_title_id
-	FOREIGN KEY (title_id) REFERENCES Employees(emp_title_id)
+FOREIGN KEY (title_id) REFERENCES Employees(emp_title_id)
 );
