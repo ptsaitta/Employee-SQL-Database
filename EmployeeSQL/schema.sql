5ccd36dc057f70c97241a2f7ebcb342396d3e482
Patrick Saitta <ptsaitta@gmail.com>
2020-12-11 14:37:59 -0500

    updated foreign key constraint syntax in schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -8,12 +8,18 @@ CREATE TABLE Department_Employees (
 dept_no varchar(6) FK >- Departments.dept_no,
 emp_no int FK >- Employees.emp_no,
 PRIMARY KEY (dept_no, emp_no)
+CONSTRAINT fk_dept_no
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+CONSTRAINT fk_emp_no
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
 CREATE TABLE Department_Manager (
 dept_no varchar(6) FK >- Departments.dept_no,
 emp_no int,
-PRIMARY KEY (dept_no, emp_no)
+PRIMARY KEY (dept_no, emp_no),
+CONSTRAINT fk_dept_no
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
 );
 
 CREATE TABLE Employees (
@@ -30,11 +36,15 @@ PRIMARY KEY (emp_no)
 CREATE TABLE Salaries (
 emp_no int FK >- Employees.emp_no,
 salary int,
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no),
+CONSTRAINT fk_emp_no
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
 CREATE TABLE Titles (
 title_id varchar(5) FK >- Employees.emp_title_id,
 title string,
-PRIMARY KEY (title_id)
+PRIMARY KEY (title_id),
+CONSTRAINT fk_title_id
+	FOREIGN KEY (title_id) REFERENCES Employees(emp_title_id)
 );
