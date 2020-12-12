b842231a2f61d2b79aaca3679bb841bd7a0ec4af
Patrick Saitta <ptsaitta@gmail.com>
2020-12-11 20:02:33 -0500

    finished first data analysis question

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,5 @@
+---List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.sex, Salaries.salary
+FROM Employees
+INNER JOIN Salaries on Employees.emp_no = Salaries.emp_no
