c752a506b6fd388719c0de54800c485a57b7ddbd
Patrick Saitta <ptsaitta@gmail.com>
2020-12-11 19:03:54 -0500

    added NOT NULLs

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -6,47 +6,47 @@ DROP TABLE IF EXISTS Salaries CASCADE;
 DROP TABLE IF EXISTS Titles CASCADE;
 
 CREATE TABLE Titles (
-title_id varchar(5),
-title VARCHAR(100),
+title_id varchar(5) NOT NULL,
+title VARCHAR(100) NOT NULL,
 PRIMARY KEY (title_id)
 );
 
 CREATE TABLE Employees (
-emp_no int,
-emp_title_id varchar(5),
-birth_date date,
-first_name varchar(30),
-last_name varchar(30),
-sex varchar(1),
-hire_date date,
+emp_no int NOT NULL,
+emp_title_id varchar(5) NOT NULL,
+birth_date date NOT NULL,
+first_name varchar(30) NOT NULL,
+last_name varchar(30) NOT NULL,
+sex varchar(1) NOT NULL,
+hire_date date NOT NULL,
 PRIMARY KEY (emp_no),
 FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
 );
 
 CREATE TABLE Departments (
-dept_name varchar(100),
-dept_no varchar(6),
+dept_name varchar(100) NOT NULL,
+dept_no varchar(6) NOT NULL,
 PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE Department_Employees (
-dept_no varchar(6),
-emp_no int,
+dept_no varchar(6) NOT NULL,
+emp_no int NOT NULL,
 PRIMARY KEY (dept_no, emp_no),
 FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
 FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
 CREATE TABLE Department_Manager (
-dept_no varchar(6),
-emp_no int,
+dept_no varchar(6) NOT NULL,
+emp_no int NOT NULL,
 PRIMARY KEY (dept_no, emp_no),
 FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
 );
 
 CREATE TABLE Salaries (
-emp_no int,
-salary int,
+emp_no int NOT NULL,
+salary int NOT NULL,
 PRIMARY KEY (emp_no),
 FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
